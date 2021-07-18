68da2d1ed530def005e7a885923d51107ee2745e
sleepturtle2 <sayantan.mukh98@gmail.com>
2021-07-18 09:23:45 +0530

    added title to navbar

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const hbs = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+//setup view engine
+app.engine('handlebars', hbs({}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (request, response) => {
+    response.render('home', {
+        title: 'Home'
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about', {
+        title: 'About'
+    });
+});
+
+app.get('/contact', (request, response) => {
+    response.render('contact', {
+        title: 'Contact'
+    });
+})
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
+})
